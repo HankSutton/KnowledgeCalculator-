c204e98ed65b3b4f3e81635dea76d8d2ac81ef04
Hank Sutton <hms802@gmail.com>
2022-10-17 16:22:03 -0400

    commit frame

--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -20,8 +20,8 @@ namespace KnowledgeCheck1_Calculator
             {
                 case "1":
                     Console.WriteLine("Enter 2 integers to add");
-                    var addNumber1 = Console.ReadLine();
-                    var addNumber2 = Console.ReadLine();
+                    string addNumber1, addNumber2;
+                    getInputs(out addNumber1, out addNumber2);
 
                     if (int.TryParse(addNumber1, out int addNumOne) && int.TryParse(addNumber2, out int addNumTwo))
                     {
@@ -36,8 +36,8 @@ namespace KnowledgeCheck1_Calculator
 
                 case "2":
                     Console.WriteLine("Enter 2 integers to subtract");
-                    var subtractNumber1 = Console.ReadLine();
-                    var subtractNumber2 = Console.ReadLine();
+                    string subtractNumber1, subtractNumber2;
+                    getInputs(out subtractNumber1, out subtractNumber2);
 
                     if (int.TryParse(subtractNumber1, out int subNumOne) && int.TryParse(subtractNumber2, out int subNumTwo))
                     {
@@ -51,14 +51,33 @@ namespace KnowledgeCheck1_Calculator
                     break;
 
                 case "3":
-                    // Add code here
+                    Console.WriteLine("Enter 2 integers to multiply");
+                    string multiplyNumber1, multiplyNumber2;
+                    getInputs(out multiplyNumber1, out multiplyNumber2);
+                   
+
+                    if (int.TryParse(multiplyNumber1, out int multiplyNumOne) && int.TryParse(multiplyNumber2, out int multiplyNumTwo))
+                        
+
+                        {
+                        Console.Write($"{multiplyNumber1} * {multiplyNumber2}= ");
+                        Console.Write(calculator.Multiply(multiplyNumOne, multiplyNumTwo));
+                    }
+
+                    else
+                    { 
+                          Console.WriteLine("One or more of the numbers is not an int");
+
+                    }
+                    
                     break;
 
                 case "4":
                     Console.WriteLine("Enter 2 integers to divide");
-                    var divideNumber1 = Console.ReadLine();
-                    var divideNumber2 = Console.ReadLine();
+                    string divideNumber1, divideNumber2;
+                    getInputs(out divideNumber1, out divideNumber2);
 
+                  
                     if (double.TryParse(divideNumber1, out double divNumOne) && double.TryParse(divideNumber2, out double divNumTwo))
                     {
                         Console.Write($"{divideNumber1} / {divideNumber2} = ");
@@ -69,12 +88,17 @@ namespace KnowledgeCheck1_Calculator
                         Console.WriteLine("One or more of the numbers is not an int");
                     }
                     break;
-                    break;
 
                 default:
                     Console.WriteLine("Unknown input");
                     break;
             }
+        }
+
+        private static void getInputs(out string number1, out string number2)
+        {
+            number1 = Console.ReadLine();
+            number2 = Console.ReadLine();
         }
     }
 }
